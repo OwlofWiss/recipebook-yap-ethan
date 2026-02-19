928b6998f9a95a0378081ff07a58c5a65849f568
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-02-19 23:17:23 +0800

    Add Ingredient and Recipe models with URLs

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ingredient_detail", args=[str(self.id)])
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("recipe_detail", args=[str(self.id)])
+
 
 # Create your models here.
+
