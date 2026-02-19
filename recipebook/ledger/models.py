f4676237c3b3ebe4c392b305c123a5c8cb036ad4
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-02-19 23:18:55 +0800

    Update models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
@@ -22,5 +22,20 @@ class Recipe(models.Model):
         return reverse("recipe_detail", args=[str(self.id)])
 
 
-# Create your models here.
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="recipe_ingredients"
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="ingredient_recipes"
+    )
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient} for {self.recipe}"
+
 
