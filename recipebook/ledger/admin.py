1e5016b100a44f52fbe11ebe812461884a11e896
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-03-02 23:42:33 +0800

    Refactor admin.py to include Profile model

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "user") 
+    search_fields = ("name",)
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
 
-
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", "author", "created_on")
     inlines = [RecipeIngredientInline]
-
+    list_filter = ("author", "created_on")
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
-
 
 @admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
