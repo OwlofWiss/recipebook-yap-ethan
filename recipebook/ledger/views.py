b753235ac43a8bea8ab68a87fbdfec591b7f3f1d
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-03-02 23:38:50 +0800

    Enhance recipe views with author and ingredient details
    Refactor recipe_list to include author and add page title. Update recipe_detail to use prefetch_related for ingredients and include recipe name in context.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
+from .models import Recipe
 
 def recipe_list(request):
-    recipes = Recipe.objects.all()
-    return render(request, "recipes/list.html", {"recipes": recipes})
+    
+    recipes = Recipe.objects.select_related('author').all()
+    
+    return render(request, "recipes/list.html", {
+        "recipes": recipes,
+        "page_title": "Recipe List"
+    })
 
-
+@login_required
 def recipe_detail(request, pk):
-    recipe = get_object_or_404(Recipe, pk=pk)
-    return render(request, "recipes/detail.html", {"recipe": recipe})
+    recipe = get_object_or_404(
+        Recipe.objects.prefetch_related('recipe_ingredients__ingredient'), 
+        pk=pk
+    )
+    
+    return render(request, "recipes/detail.html", {
+        "recipe": recipe,
+        "page_title": recipe.name  
+    })
+
