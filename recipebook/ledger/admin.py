1ed5408ad0f672e3bc4f8f6804e0709dadb156de
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-03-12 23:04:07 +0800

    Refactor admin.py to include Profile and RecipeImage

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,21 +1,29 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient, Profile, RecipeImage
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "user") 
+    search_fields = ("name",)
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+    extra = 1
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    inlines = [RecipeIngredientInline]
-
+    list_display = ("name", "author", "created_on")
+    list_filter = ("author", "created_on")
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
-
 
 @admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
