75affb85850b838b7f9e36b7e987e15d757feb9c
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-03-12 23:05:07 +0800

    Refactor models to include Profile and RecipeImage

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
+# Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
@@ -11,16 +24,21 @@ class Ingredient(models.Model):
     def get_absolute_url(self):
         return reverse("ingredient_detail", args=[str(self.id)])
 
-
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="recipes"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("recipe_detail", args=[str(self.id)])
-
+        return reverse("recipe_detail", args=[self.pk])
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
@@ -37,5 +55,18 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient} for {self.recipe}"
+    
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='recipe_images/', blank=False, null=False)
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE, 
+        related_name="images"
+    )
+
+    def __str__(self):
+        return f"Image for {self.recipe.name}"
+
 
 
