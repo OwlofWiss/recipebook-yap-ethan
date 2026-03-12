3dd5a96c53104f7a663c63c84417b5445273674f
OwlofWiss <ethan.yap@student.ateneo.edu>
2026-03-12 23:38:07 +0800

    Add recipe creation and image upload views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
-from .models import Recipe
+from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import get_object_or_404, redirect, render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from .models import Recipe, RecipeImage
+
 
 def recipe_list(request):
     
@@ -22,4 +26,30 @@ def recipe_detail(request, pk):
         "recipe": recipe,
         "page_title": recipe.name  
     })
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name'] 
+    template_name = "recipes/recipe_form.html"
+    success_url = reverse_lazy('recipe_list')
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    fields = ['image', 'description']
+    template_name = "recipes/image_form.html"
+
+    def form_valid(self, form):
+        recipe_pk = self.kwargs.get('pk')
+        form.instance.recipe = get_object_or_404(Recipe, pk=recipe_pk)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('recipe_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = get_object_or_404(Recipe, pk=self.kwargs.get('pk'))
+        return context
